77c1aeca0ae2c0aee8437c449848a53f3aca4986
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-07 16:36:11 +0100

    Add project files.

---
+++ IEnumerableAndIEnumerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IEnumerableAndIEnumerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

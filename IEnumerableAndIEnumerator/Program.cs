13bfd01cff6996c5fcd6fe890542dd64794620cb
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-08 09:00:45 +0100

    IEnumerable Example 1

--- IEnumerableAndIEnumerator/Program.cs
+++ IEnumerableAndIEnumerator/Program.cs
@@ -25,18 +25,89 @@ namespace IEnumerableAndIEnumerator
     {
         static void Main(string[] args)
         {
-            DogShelter shelter = new DogShelter();
+            // DogShelter shelter = new DogShelter();
 
-            foreach (Dog dog in shelter)
+            // foreach (Dog dog in shelter)
+            // {
+            //     if (!dog.IsNaughtyDog)
+            //     {
+            //         dog.GiveTreat(2);
+            //     }
+            //     else
+            //     {
+            //         dog.GiveTreat(1);
+            //     }
+            // }
+
+            // Creating a generic IEnumerable variable that takes any collection of type int.
+            // We will use this variable to store the collections that will get returned by the GetCollection() method
+            IEnumerable<int> unknownCollection;
+            unknownCollection = GetCollection(1);
+
+            Console.WriteLine("This was a List<int>");
+            // Foreach number in the collection we got back from GetCollection(1);
+            foreach (int num in unknownCollection)
             {
-                if (!dog.IsNaughtyDog)
-                {
-                    dog.GiveTreat(2);
-                }
-                else
-                {
-                    dog.GiveTreat(1);
-                }
+                Console.WriteLine(num + " ");
+            }
+
+            // New line
+            Console.WriteLine("");
+            // Call GetCollection() with options = 2 which will return a Queue<int>
+            // But we will store it int the base type of generic collections
+            unknownCollection = GetCollection(2);
+
+            Console.WriteLine("This was a Queue<int>");
+            // For-each number in the collection we got back from GetCollection(2);
+            foreach (int num in unknownCollection)
+            {
+                Console.WriteLine(num + " ");
+            }
+
+            // New line
+            Console.WriteLine("");
+            // Call GetCollection() with options = 5 which will return an array <int>
+            // But we will store it in the base type of generic collections
+            unknownCollection = GetCollection(5);
+
+            Console.WriteLine("This was a array of int");
+            // For-each number in the collection we got back from GetCollection(5);
+            foreach (int num in unknownCollection)
+            {
+                Console.WriteLine(num + " ");
+            }
+        }
+
+        static IEnumerable<int> GetCollection(int option)
+        {
+            // Create a list of numbers and initialize it
+            List<int> numberList = new List<int>() { 1, 2, 3, 4, 5 };
+
+            Queue<int> numberQueue = new Queue<int>();
+            // Add values to the queue
+            numberQueue.Enqueue(6);
+            numberQueue.Enqueue(7);
+            numberQueue.Enqueue(8);
+            numberQueue.Enqueue(9);
+            numberQueue.Enqueue(10);
+
+            // If the options is 1
+            if (option == 1)
+            {
+                // Return the list of type List<int>
+                return numberList;
+            }
+            // If the options is 2
+            else if (option == 2)
+            {
+                // Return the queue of type<int>
+                return numberQueue;
+            }
+            // Otherwise
+            else
+            {
+                // Return an array of numbers initialized with some numbers
+                return new int[] { 11, 12, 13, 14, 15 };
             }
         }
     }
